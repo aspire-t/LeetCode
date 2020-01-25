e24744b4e854596e470ecd0086b3e0d14fd7b6e8
aspire-t <zhijianwubu@gmail.com>
2020-01-25 17:43:45 +0800

    914

--- code/array/914.js
+++ code/array/914.js
@@ -1,27 +1,33 @@
 // 914. 卡牌分组
-export default (arr) => {
+// export default (arr) => {
+var hasGroupsSizeX = function (arr) {
   // 存储每张卡牌的总数
-  // 修改排序的方式修改为直接统计每个相同字符的数量，思路不变（LeetCode测试用例）
   let group = []
   let tmp = {}
+  // 统计每个相同字符的数量
   arr.forEach(item => {
     tmp[item] = tmp[item] ? tmp[item] + 1 : 1
   })
+  // Object.values 用于取出有效值，可以去除undefined
   for (let v of Object.values(tmp)) {
     group.push(v)
   }
   // 此时group已经存放的是每张牌的总数了（数组只遍历一遍，避免了排序和正则的耗时）
+
   // 求两个数的最大公约数
   let gcd = (a, b) => {
     if (b === 0) {
       return a
     } else {
+      // 递归求最大公约数
       return gcd(b, a % b)
     }
   }
+
   while (group.length > 1) {
-    let a = group.shift()
+    let a = group.shift() // shift 取数组第一个值
     let b = group.shift()
+
     let v = gcd(a, b)
     if (v === 1) {
       return false
@@ -31,3 +37,5 @@ export default (arr) => {
   }
   return group.length ? group[0] > 1 : false
 }
+
+hasGroupsSizeX([1, 1, 1, 1, 2, 2, 2, 2, 2, 2])
