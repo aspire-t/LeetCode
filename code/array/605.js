cb1f4e049836475313d5378ec3b983c38713e600
aspire-t <zhijianwubu@gmail.com>
2020-01-26 14:21:22 +0800

    605

---
+++ code/array/605.js
@@ -0,0 +1,24 @@
+// 605. 种花问题
+
+var canPlaceFlowers = function (flowerbed, n) {
+  let count = 0
+  // 右边界补充[0,0,0],最后一块地能不能种只取决于前面的是不是1，所以默认最后一块地的右侧是0
+  flowerbed.push(0)
+  for (let i = 0; i < flowerbed.length - 1; i++) {
+    if (flowerbed[i] === 0) {
+
+      if (i == 0 && flowerbed[1] === 0) {
+        count++
+        i++
+      } else if (flowerbed[i - 1] === 0 && flowerbed[i + 1] === 0) {
+        count++
+        i++
+      }
+      console.log(count, i)
+    }
+  }
+
+  return count >= n
+};
+
+console.log(canPlaceFlowers([0, 1, 0], 1))
