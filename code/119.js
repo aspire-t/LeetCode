abf9828108d51f10434e1355098c2d4529b5cae1
aspire-t <zhijianwubu@gmail.com>
2020-01-30 14:33:07 +0800

    119

---
+++ code/119.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function (rowIndex) {
+  if (rowIndex == 0) {
+    return [1]
+  }
+
+  let result = []
+  for (let i = 1; i < rowIndex + 1; i++) {
+    const lastRow = result // 上一行的值
+    const row = []
+
+    row.push(1)
+
+    for (let j = 1; j < i; j++) {
+      row.push(lastRow[j - 1] + lastRow[j])
+    }
+
+    row.push(1)
+    result = row
+  }
+
+  return result
+};
+
+console.log(getRow(5))
